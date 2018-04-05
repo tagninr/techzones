c323d71f6d1bcf3c7901624f59df6cd6878684e9
TagNinr_Nguyen <tagninr.nguyen@gmail.com>
2018-04-05 15:34:06 +0700

    Update file database

--- techzones.sql
+++ techzones.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2018 at 08:40 AM
+-- Generation Time: Apr 05, 2018 at 10:29 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -30,9 +30,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `attributes` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `attribute_set_id` int(10) UNSIGNED NOT NULL,
-  `product_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -45,8 +44,22 @@ CREATE TABLE `attributes` (
 
 CREATE TABLE `attribute_sets` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(191) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `category_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attribute_values`
+--
+
+CREATE TABLE `attribute_values` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
+  `attribute_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -61,7 +74,7 @@ CREATE TABLE `bills` (
   `id` int(10) UNSIGNED NOT NULL,
   `date_order` date NOT NULL,
   `total` double(8,2) NOT NULL,
-  `note` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `note` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `customer_id` int(10) UNSIGNED NOT NULL,
   `employees_id` int(10) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -94,7 +107,6 @@ CREATE TABLE `brands` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci,
-  `parent_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -176,8 +188,10 @@ CREATE TABLE `images` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `url` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `product_id` int(10) UNSIGNED NOT NULL,
-  `attribute_id` int(10) UNSIGNED NOT NULL,
+  `brand_id` int(10) UNSIGNED DEFAULT NULL,
+  `category_id` int(10) UNSIGNED DEFAULT NULL,
+  `product_id` int(10) UNSIGNED DEFAULT NULL,
+  `attribute_value_id` int(10) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -206,12 +220,13 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2018_04_03_031411_create_cities_table', 1),
 (6, '2018_04_03_031429_create_customers_table', 1),
 (7, '2018_04_03_031557_create_employees_table', 1),
-(8, '2018_04_03_031632_create_images_table', 1),
-(9, '2018_04_03_031852_create_products_table', 1),
-(10, '2018_04_03_031941_create_bills_table', 1),
-(11, '2018_04_03_032119_create_bill_details_table', 1),
-(12, '2018_04_03_043801_create_attribute_sets_table', 1),
-(13, '2018_04_03_043814_create_attributes_table', 1);
+(8, '2018_04_03_031852_create_products_table', 1),
+(9, '2018_04_03_031941_create_bills_table', 1),
+(10, '2018_04_03_032119_create_bill_details_table', 1),
+(11, '2018_04_03_043801_create_attribute_sets_table', 1),
+(12, '2018_04_03_043814_create_attributes_table', 1),
+(13, '2018_04_04_094857_create_attribute_values_table', 1),
+(14, '2018_04_04_100109_create_images_table', 1);
 
 -- --------------------------------------------------------
 
@@ -269,25 +284,39 @@ CREATE TABLE `users` (
 -- Indexes for table `attributes`
 --
 ALTER TABLE `attributes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `attributes_attribute_set_id_foreign` (`attribute_set_id`);
 
 --
 -- Indexes for table `attribute_sets`
 --
 ALTER TABLE `attribute_sets`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `attribute_sets_category_id_foreign` (`category_id`);
+
+--
+-- Indexes for table `attribute_values`
+--
+ALTER TABLE `attribute_values`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `attribute_values_product_id_foreign` (`product_id`),
+  ADD KEY `attribute_values_attribute_id_foreign` (`attribute_id`);
 
 --
 -- Indexes for table `bills`
 --
 ALTER TABLE `bills`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bills_customer_id_foreign` (`customer_id`),
+  ADD KEY `bills_employees_id_foreign` (`employees_id`);
 
 --
 -- Indexes for table `bill_details`
 --
 ALTER TABLE `bill_details`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bill_details_bill_id_foreign` (`bill_id`),
+  ADD KEY `bill_details_product_id_foreign` (`product_id`);
 
 --
 -- Indexes for table `brands`
@@ -301,7 +330,8 @@ ALTER TABLE `brands`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `categories_name_unique` (`name`);
+  ADD UNIQUE KEY `categories_name_unique` (`name`),
+  ADD KEY `categories_parent_id_foreign` (`parent_id`);
 
 --
 -- Indexes for table `cities`
@@ -315,7 +345,8 @@ ALTER TABLE `cities`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `customers_email_unique` (`email`);
+  ADD UNIQUE KEY `customers_email_unique` (`email`),
+  ADD KEY `customers_city_id_foreign` (`city_id`);
 
 --
 -- Indexes for table `employees`
@@ -329,7 +360,11 @@ ALTER TABLE `employees`
 --
 ALTER TABLE `images`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `images_name_unique` (`name`);
+  ADD UNIQUE KEY `images_name_unique` (`name`),
+  ADD KEY `images_brand_id_foreign` (`brand_id`),
+  ADD KEY `images_category_id_foreign` (`category_id`),
+  ADD KEY `images_product_id_foreign` (`product_id`),
+  ADD KEY `images_attribute_value_id_foreign` (`attribute_value_id`);
 
 --
 -- Indexes for table `migrations`
@@ -348,7 +383,10 @@ ALTER TABLE `password_resets`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `products_name_unique` (`name`);
+  ADD UNIQUE KEY `products_name_unique` (`name`),
+  ADD KEY `products_brand_id_foreign` (`brand_id`),
+  ADD KEY `products_category_id_foreign` (`category_id`),
+  ADD KEY `products_employees_id_foreign` (`employees_id`);
 
 --
 -- Indexes for table `users`
@@ -371,6 +409,12 @@ ALTER TABLE `attributes`
 -- AUTO_INCREMENT for table `attribute_sets`
 --
 ALTER TABLE `attribute_sets`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attribute_values`
+--
+ALTER TABLE `attribute_values`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -425,7 +469,7 @@ ALTER TABLE `images`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -438,6 +482,72 @@ ALTER TABLE `products`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attributes`
+--
+ALTER TABLE `attributes`
+  ADD CONSTRAINT `attributes_attribute_set_id_foreign` FOREIGN KEY (`attribute_set_id`) REFERENCES `attribute_sets` (`id`);
+
+--
+-- Constraints for table `attribute_sets`
+--
+ALTER TABLE `attribute_sets`
+  ADD CONSTRAINT `attribute_sets_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `attribute_values`
+--
+ALTER TABLE `attribute_values`
+  ADD CONSTRAINT `attribute_values_attribute_id_foreign` FOREIGN KEY (`attribute_id`) REFERENCES `attributes` (`id`),
+  ADD CONSTRAINT `attribute_values_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `bills`
+--
+ALTER TABLE `bills`
+  ADD CONSTRAINT `bills_customer_id_foreign` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`),
+  ADD CONSTRAINT `bills_employees_id_foreign` FOREIGN KEY (`employees_id`) REFERENCES `employees` (`id`);
+
+--
+-- Constraints for table `bill_details`
+--
+ALTER TABLE `bill_details`
+  ADD CONSTRAINT `bill_details_bill_id_foreign` FOREIGN KEY (`bill_id`) REFERENCES `bills` (`id`),
+  ADD CONSTRAINT `bill_details_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `categories`
+--
+ALTER TABLE `categories`
+  ADD CONSTRAINT `categories_parent_id_foreign` FOREIGN KEY (`parent_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `customers`
+--
+ALTER TABLE `customers`
+  ADD CONSTRAINT `customers_city_id_foreign` FOREIGN KEY (`city_id`) REFERENCES `cities` (`id`);
+
+--
+-- Constraints for table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_attribute_value_id_foreign` FOREIGN KEY (`attribute_value_id`) REFERENCES `attribute_values` (`id`),
+  ADD CONSTRAINT `images_brand_id_foreign` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  ADD CONSTRAINT `images_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `images_product_id_foreign` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_brand_id_foreign` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`),
+  ADD CONSTRAINT `products_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `products_employees_id_foreign` FOREIGN KEY (`employees_id`) REFERENCES `employees` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
